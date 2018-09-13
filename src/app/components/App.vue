b796f107a1188e93efd75eab047ed1f27b3bbe77
migohe14 <migohe14@gmail.com>
2018-09-13 12:10:06 +0200

    fetch

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -8,11 +8,18 @@
             <div class="col-md-5">
                 <div class="card">
                     <div class="card-body">
-                        <form>
+                        <form @submit.prevent="addTask">
                             <div class="form-group">
-                                <input type="text" placeholder="Escribe un mensaje aquí" class="form-control">
+                                <input type="text" v-model="task.title" placeholder="Escribe el título aquí" class="form-control">
 
                             </div>
+                            <div class="form-group">
+                                <textarea cols="30" rows="10" class="form-control" v-model="task.description" placeholder="Escribe la descripción aquí">
+
+                                </textarea>
+                            </div>
+
+                            <button class="btn btn-primary btn-block">Enviar</button>
                         </form>
                     </div>
                 </div>
@@ -21,3 +28,47 @@
     </div>
 </div>
 </template>
+
+<script>
+
+// class Task {
+//     constructor(title, description){
+//         this.title = title;
+//         this.description = description;
+//     }
+// }
+export default {
+    data() {
+        return {
+          task: {
+            title:'',
+            description: ''
+          }
+          
+        }
+    },
+
+    methods: {
+        addTask() {
+            // this.task = new Task()
+
+            fetch('/api/tasks', {
+                method: 'POST',
+                body: JSON.stringify(this.task),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(data => console.log(data))
+        //    console.log(this.task)
+           
+
+            this.task.title = '';
+            this.task.description = '';
+        }
+    }
+}
+</script>
+
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //Routes
-app.use('/tasks', require('./routes/tasks'));
+app.use('/api/tasks', require('./routes/tasks'));
 
 
 //Static Files
